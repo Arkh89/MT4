a3bd27b39d9bcbf7f5f6336a88a08b33399042e8
Charles Mignon <mignon@kth.se>
2012-03-04 01:20:32 +0100

    Début_à_continuer

--- src/segment.cpp
+++ src/segment.cpp
@@ -1,24 +1,85 @@
-// J ai commencé mais marche pas pour l'instant les  constructeurs.... enfin...!
+// Le calcul de la normale est fait dans les constructeurs (je suis pas sûr que ce soit le bon endroit ?)
+// Et deuxième point c'est un vecteur unitaire d'origine en zéro du coup
 
 #include <iostream>
 #include "segment.hpp"
 
-Segment::Segment(void)
+
+
+Segment::Segment(void):v1(),v2(),n()
 {
-	v1 = new Vect2D(void); 
-	v2 = new Vect2D(void);
-	// We probably don't need to use pointers to Vect2D in this object since we know that a segment MUST have two vectors as members (this is not optional ;) ).
-	// Thus, the members must be Vect2D objects (no reference/pointer) and we have to use initialization lists to init our objects (if necessary, so not in this constructor but in the two others)
 }
 
-Segment::Segment(int _x1,int _x2,int _y1,int _y2)
+Segment::Segment(int _x1,int _x2,int _y1,int _y2):v1(_x1,_y1),v2(_x2,_y2)
 {
-	v1 = new Vect2D(int _x1,int _x2);
-	v2 = new Vect2D(int _y1,int _y2);
+	TValue D = (_y2 - _y1) / (_x2 - _x1);
+	n.x = - ( D ) / sqrt(1 + D*D);
+	n.y = + ( 1 ) / sqrt(1 + D*D);
 }
 
-Segment::Segment(const Vect2D& vect1, const Vect2D& vect2)
+Segment::Segment(const Vect2D& vect1, const Vect2D& vect2):v1(vect1),v2(vect2)
 {
-	v1 = new Vect2D(*vect1); // warning : you declared your arguments as 'reference to a Vect2D', thus you don't need to use * to dereference/access the object (as in C for the pointers). That's why we love C++!
-	v2 = new Vect2D(*vect2);
+	TValue D = (v2.y - v1.y) / (v2.x - v1.x);
+	n.x = - ( D ) / sqrt(1 + D*D);
+	n.y = + ( 1 ) / sqrt(1 + D*D);
 }
+
+
+
+TValue Segment::distance(const Segment& seg) const
+{
+	TValue dist = sqrt( (v2.x - v1.x)*(v2.x - v1.x) + (v2.y - v1.y)*(v2.y - v1.y) );
+	return dist;
+}
+
+
+TValue Segment::distSeg(const Vect2D& v) const
+{
+	Vect2D segV( v2.x - v1.x, v2.y - v1.y );
+	TValue D = segV.y / segV.y;
+	TValue DSeg = segV.Vect2D::scalarProd(v) / sqrt(1 + D*D);
+	return DSeg;
+}
+
+
+
+// Pour les operateurs += et -= comme ça je vois pas trop l'intérêt ni la signification avec segment parce que l'angle
+// n'est pas conservé... (idée?)
+// Et pour *= et /= uniquement par un scalaire ( je vois pas la sgnification de multiplier deux segments ou encore de les diviser entre eux).
+// Mais pour la multiplication ou la division par un scalaire j'ai deux versions qui me viennent, une a priori bête on multiplie
+// les deux vecteurs par s du coup le segment conserve l'angle et s-uple sa norme mais n' plus la même origine. Ou la version deux
+// que j'imagine plus on conserve angle et origine mais on s-uple la norme
+
+// Version 1
+
+Segment& Segment::operator*=(TValue s)
+{
+	v1 *= s;
+	v2 *= s;
+	return *this;
+}
+
+Segment& Segment::operator/=(TValue s)
+{
+	v1 /= s;
+	v2 /= s;
+	return *this;
+}
+
+// Version 2
+// Segment& Segment::operator*=(TValue s)
+// {
+// 	v2.x += (v2.x - v1.x)*s;
+// 	v2.y += (v2.y - v1.y)*s;
+// 	return *this;
+// }
+
+// Segment& Segment::operator/=(TValue s)
+// {
+// 	v2.x += (v2.x - v1.x)/s;
+// 	v2.y += (v2.y - v1.y)/s;
+// 	return *this;
+// }
+
+
+
--- src/segment.hpp
+++ src/segment.hpp
@@ -2,12 +2,12 @@
 #define SEGMENT_HPP_INCLUDED
 
 #include <iostream>
+#include <cmath>
 #include "vect2D.hpp"
 
 
 using namespace std;
 
-typedef float TValue; // Not needed here because we already included header file vect2D.hpp which contains this definition.
 
 class Segment
 {
@@ -16,13 +16,17 @@ class Segment
 		Segment(int _x1,int _x2,int _y1,int _y2);
 		Segment(const Vect2D& vect1, const Vect2D& vect2);
 
-		virtual ~Segment(void);
-		// I have never used a virtual destructor in my whole life. Please don't do that too, unless you know *exactly* what you are doing. And, I agree that in the current design with dynamically set objects we have to clean them in the desctuctor but we won't need it if we use directly the object (see next comment about that) because the call is implicit.
-		
+		TValue distance(const Segment& seg) const; // taille du segment
+		TValue distSeg(const Vect2D& v) const; // Distance d'un point au segment
+
+		Segment& operator*=(TValue s);
+		Segment& operator/=(TValue s);
+
+
 	protected:
 
 	private:
-	Vect2D* v1,v2,n; //We don't need dynamic object here, just use Vect2D v1,v2,n and the initialization list constructor(arguments) : initializationList
+		Vect2D v1,v2,n;
 };
 
 #endif // SEGMENT_HPP_INCLUDED
