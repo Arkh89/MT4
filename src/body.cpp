87211fa4f37d112ec7e6d3d08cec4356c04ccf7e
Charles Mignon <mignon@kth.se>
2012-03-13 17:38:13 +0100

    Adding Body class

---
+++ src/body.cpp
@@ -0,0 +1,39 @@
+#include "body.hpp"
+#include <cmath>
+
+double Body::gN = 9.81;
+
+
+Body::Body(void)
+{ }
+
+Body::Body(Vect2D pos0, double _m, double _k)
+ : pos(pos0), m(_m), k(_k)
+{ }
+
+Body::Body(Vect2D pos0, Vect2D v0, double _m, double _k)
+ : pos(pos0), v(v0), m(_m), k(_k)
+{ }
+
+Body::~Body(void)
+{ }
+
+
+Vect2D Body::trajectory(double t)
+{
+	Vect2D Dv;
+	Dv.x = (m/k) * (v.x) * (1 - exp(-(k/m)*t)) + (R.x/k) * t;
+	Dv.y = (m/k) * ((m/k)*gN + v.y) * (1 - exp(-(k/m)*t))  -  (m/k) * gN * t  + (R.y/k) * t;
+	return Dv;
+}
+
+Vect2D Body::getCurPos(double t)
+{
+	return pos += trajectory(t);
+}
+
+
+void Body::setGrav(double gNew)
+{
+	gN = gNew;
+}
---
+++ src/body.hpp
@@ -0,0 +1,28 @@
+#ifndef BODY_HPP_INCLUDED
+#define BODY_HPP_INCLUDED
+#include "vect2D.hpp"
+
+using namespace std;
+
+
+class Body
+{
+	public:
+
+		Body(void);
+		Body(Vect2D pos0, double _m, double _k);
+		Body(Vect2D pos0, Vect2D v0, double _m, double _k);
+		~Body(void);
+
+		Vect2D trajectory(double t);
+		Vect2D getCurPos(double t);
+
+		static void setGrav(double gNew); // Pour redéfinir la constante de gravitation => Possibilité de gameplay
+
+	private:
+		Vect2D pos,v,R; // Position & Speed
+		double m,k; // Mass & friction coefficient
+		static double gN; // Gravitation constant 9,81 m.s-2
+};
+
+#endif // BODY_HPP_INCLUDED
