e649e66cc1ee5b62b028c517647772c00b1419a4
Arkh89 <ronna.kerviche@free.fr>
2012-03-19 13:52:29 -0700

    Adding keyboard management, fixing ratio issue, adding renderer movement with arrows

---
+++ src/keyLayout.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include "keyLayout.hpp"
+
+	KeyLayout::KeyLayout(QWidget* w)
+	 : QWidget(w)
+	{
+		for(int i=0; i<NumKey; i++)
+		{
+			keyPressed[i]		= false;
+			keyJustPressed[i]	= false;
+			keyJustReleased[i]	= false;
+		}
+
+		keyAssociation[static_cast<int>(KeyUp)]		= Qt::Key_Up;
+		keyAssociation[static_cast<int>(KeyDown)]	= Qt::Key_Down;
+		keyAssociation[static_cast<int>(KeyLeft)]	= Qt::Key_Left;
+		keyAssociation[static_cast<int>(KeyRight)]	= Qt::Key_Right;
+		keyAssociation[static_cast<int>(KeySpace)]	= Qt::Key_Space;
+		keyAssociation[static_cast<int>(KeyEscape)]	= Qt::Key_Escape;
+	}
+
+	KeyLayout::~KeyLayout(void)
+	{ }
+
+	KeyAction KeyLayout::corresponding(const Qt::Key& k) const
+	{
+		for(int i=0; i<NumKey; i++)
+			if(keyAssociation[i]==k)
+				return static_cast<KeyAction>(i);
+
+		return NoAction;
+	}
+
+	bool KeyLayout::pressed(const KeyAction& a) const
+	{
+		return keyPressed[static_cast<int>(a)];
+	}
+
+	bool KeyLayout::justPressed(const KeyAction& a)
+	{
+		if(keyJustPressed[static_cast<int>(a)])
+		{
+			keyJustPressed[static_cast<int>(a)] = false;
+			return true;
+		}
+		else
+			return false;
+	}
+
+	bool KeyLayout::justReleased(const KeyAction& a)
+	{
+		if(keyJustReleased[static_cast<int>(a)])
+		{
+			keyJustReleased[static_cast<int>(a)] = false;
+			return true;
+		}
+		else
+			return false;
+	}
+
+	void KeyLayout::keyPress(QKeyEvent* event)
+	{
+		if(!event->isAutoRepeat())
+		{
+			KeyAction a = corresponding(static_cast<Qt::Key>(event->key()));
+
+			std::cout << "Event : Key pressed" << std::endl;
+
+			if(a!=NoAction)
+			{
+				keyPressed[static_cast<int>(a)] = true;
+				keyJustPressed[static_cast<int>(a)] = true;
+				keyJustReleased[static_cast<int>(a)] = false;
+			}
+			else
+				std::cerr << "Warning : Key not assiociated" << std::endl;
+		}
+	}
+
+	void KeyLayout::keyRelease(QKeyEvent* event)
+	{
+		if(!event->isAutoRepeat())
+		{
+			KeyAction a = corresponding(static_cast<Qt::Key>(event->key()));
+
+			std::cout << "Event : Key released" << std::endl;
+
+			if(a!=NoAction)
+			{
+				keyPressed[static_cast<int>(a)] = false;
+				keyJustPressed[static_cast<int>(a)] = false;
+				keyJustReleased[static_cast<int>(a)] = true;
+			}
+			else
+				std::cerr << "Warning : Key not assiociated" << std::endl;
+		}
+	}
---
+++ src/keyLayout.hpp
@@ -0,0 +1,44 @@
+#ifndef __MT4_KEYLAYOUT__
+#define __MT4_KEYLAYOUT__
+
+	#include <QKeyEvent>
+	#include <QWidget>
+
+	enum KeyAction
+	{
+		KeyUp,
+		KeyDown,
+		KeyLeft,
+		KeyRight,
+		KeySpace,
+		KeyEscape,
+		// Add new keys before this line
+		NumKey,
+		NoAction
+	};
+
+	class KeyLayout : public QWidget
+	{
+		Q_OBJECT
+
+		private :
+			Qt::Key	keyAssociation[NumKey];
+			bool 	keyPressed[NumKey],
+				keyJustPressed[NumKey],
+				keyJustReleased[NumKey];
+
+			KeyAction corresponding(const Qt::Key& k) const;
+		public :
+			KeyLayout(QWidget* w);
+			~KeyLayout(void);
+
+			bool pressed(const KeyAction& a) const;
+			bool justPressed(const KeyAction& a);
+			bool justReleased(const KeyAction& a);
+
+		public slots :
+			void keyPress(QKeyEvent* event);
+			void keyRelease(QKeyEvent* event);
+	};
+
+#endif
